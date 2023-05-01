186324201fa8f8852b29f3c35a09894c6e9efa1b
faisalahmed11 <faisalbaloch09876@gmail.com>
2023-05-01 17:18:45 +0500

    Initial Commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
 import '@/styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+<Head>
+        <title>Web practice</title>
+        <meta name="description" content="This is for web practice." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+  <Component {...pageProps} />
+  </>
 }
