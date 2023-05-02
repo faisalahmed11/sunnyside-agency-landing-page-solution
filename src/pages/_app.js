22b04e30335662f1085d7c2142e9b2d317f92779
faisalahmed11 <faisalbaloch09876@gmail.com>
2023-05-02 12:24:50 +0500

    initial commmit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,8 +4,8 @@ import Head from 'next/head'
 export default function App({ Component, pageProps }) {
   return <>
 <Head>
-        <title>Front-end QR Code Challenge</title>
-        <meta name="description" content="A finial result of a frontend qr-code-challenge given by frontendmentor.io and done by Faisal Ahmed" />
+        <title>result-summary challenge by FrontEnd mentor</title>
+        <meta name="description" content="A finial result of result-summary-challenge by Frontend Mentor" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,8 @@
 import React from 'react'
-import Image from 'next/image'
-import styles from "../styles/Home.module.css"
-import Link from 'next/link'
+
 function Home() {
   return (
-    <main className={`${styles.main}`}>
-    <div className={`${styles.card}`}>
-    <Image className={`${styles.qrCode}`} src={"/image-qr-code.png"}  alt='qr-code-image' width={500} height={500}/>
-  <h2>
-    Improve your front-end skills by building projects
-  </h2>
-  <p>Scan the QR code to visit Frontend Mentor and take your codding skills to the next level</p>
-    </div>
-  
-    <div className={`${styles.mention}`}>
-    Challenge by <Link href="https://www.frontendmentor.io/profile/faisalahmed11" target="_blank">Frontend Mentor</Link>. 
-    Coded by <Link href="https://twitter.com/FaisalAhmed_01">Faisal Ahmed</Link>.
-  </div>
-    </main>
+    <div>Home</div>
   )
 }
 
