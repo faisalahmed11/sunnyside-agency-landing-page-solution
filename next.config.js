d76830ec1c84671eb89a6ff84dca56e400ca4127
faisalahmed11 <faisalbaloch09876@gmail.com>
2023-05-25 04:54:12 +0500

    fixing build error

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   images:{
     domains:['unsplash.it']
-  }
+  },
+  // sassOptions: {
+  //   includePaths: [path.join(__dirname, 'styles')],
+  // }
 }
 
 module.exports = nextConfig
