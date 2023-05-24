9ee66e416d16bc9b66ef2afb60ec5a281d4f8342
faisalahmed11 <faisalbaloch09876@gmail.com>
2023-05-25 04:31:58 +0500

    fixing deploy build error

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import styles from "../styles/Home.module.scss";
-import Image from "next/image";
 const Accordion = ({ title, body }) => {
   const [active, setActive] = useState(false);
   return (
@@ -24,11 +23,10 @@ function Home() {
               "quis illum, voluptatem quas! Culpa repellendus tempore vitae tempora consequatur minima debitis ea dolorem ipsam reiciendis beatae dignissimos sunt itaque quasi maiores, quod eum repellat iusto ex vel, eius doloremque. Est cumque voluptates numquam eligendi quae vero perferendis at reprehenderit nulla necessitatibus? Eum reprehenderit beatae odio et rerum exercitationem asperiores culpa quasi dolorum"
             }
           />
-
           <Accordion
-            title={"ipsam reiciendis beatae dignissimos"}
+            title={"ipsam reiciendis beatae dignissimos sunt"}
             body={
-              "quis illum, voluptatem quas! Culpa repellendus tempore vitae tempora consequatur minima debitis ea dolorem ipsam reiciendis beatae dignissimos sunt itaque quasi maiores, quod eum repellat iusto ex vel, eius doloremque. Est cumque voluptates numquam eligendi quae vero perferendis at reprehenderit nulla necessitatibus? Eum reprehenderit beatae odio et rerum exercitationem asperiores culpa quasi dolorum nostrum aspernatur consequatur esse sed eos, suscipit rem similique! Doloremque dolorum accusantium exercitationem nulla et ad voluptas eum deleniti consectetur ea, error cumque tenetur id molestiae beatae autem voluptatem nesciunt in rerum eos nemo laborum quae? Fugiat unde, consequatur itaque dolores numquam consequuntur incidunt soluta deserunt illo recusandae laboriosam iusto quod natus pariatur ut! Illum, nihil tempora rem ea sunt, consequuntur non libero voluptates iste quidem itaque culpa necessitatibus? Dignissimos aspernatur perspiciatis animi! Provident laborum quisquam ut dicta maiores voluptas dolorem assumenda illo dolor veritatis facilis sit totam tempora nulla dolores quasi reiciendis mollitia in deleniti, velit, debitis quis illum, voluptatem quas! Culpa repellendus tempore vitae tempora consequatur minima debitis ea dolorem ipsam reiciendis beatae dignissimos sunt itaque quasi maiores, quod eum repellat iusto ex vel, eius doloremque. Est cumque voluptates numquam eligendi quae vero perferendis at reprehend"
+              "quis illum, voluptatem quas! Culpa repellendus tempore vitae tempora consequatur minima debitis ea dolorem ipsam reiciendis beatae dignissimos sunt itaque quasi maiores, quod eum repellat iusto ex vel, eius doloremque. Est cumque voluptates numquam eligendi quae vero perferendis at reprehenderit nulla necessitatibus? Eum reprehenderit beatae odio et rerum exercitationem asperiores culpa quasi dolorum"
             }
           />
           <Accordion
@@ -37,26 +35,26 @@ function Home() {
               "quis illum, voluptatem quas! Culpa repellendus tempore vitae tempora consequatur minima debitis ea dolorem ipsam reiciendis beatae dignissimos sunt itaque quasi maiores, quod eum repellat iusto ex vel, eius doloremque. Est cumque voluptates numquam eligendi quae vero perferendis at reprehenderit nulla necessitatibus? Eum reprehenderit beatae odio et rerum exercitationem asperiores culpa quasi dolorum"
             }
           />
-
           <Accordion
-            title={"ipsam reiciendis beatae dignissimos"}
-            body={
-              "quis illum, voluptatem quas! Culpa repellendus tempore vitae tempora consequatur minima debitis ea dolorem ipsam reiciendis beatae dignissimos sunt itaque quasi maiores, quod eum repellat iusto ex vel, eius doloremque. Est cumque voluptates numquam eligendi quae vero perferendis at reprehenderit nulla necessitatibus? Eum reprehenderit beatae odio et rerum exercitationem asperiores culpa quasi dolorum nostrum aspernatur consequatur esse sed eos, suscipit rem similique! Doloremque dolorum accusantium exercitationem nulla et ad voluptas eum deleniti consectetur ea, error cumque tenetur id molestiae beatae autem voluptatem nesciunt in rerum eos nemo laborum quae? Fugiat unde, consequatur itaque dolores numquam consequuntur incidunt soluta deserunt illo recusandae laboriosam iusto quod natus pariatur ut! Illum, nihil tempora rem ea sunt, consequuntur non libero voluptates iste quidem itaque culpa necessitatibus? Dignissimos aspernatur perspiciatis animi! Provident laborum quisquam ut dicta maiores voluptas dolorem assumenda illo dolor veritatis facilis sit totam tempora nulla dolores quasi reiciendis mollitia in deleniti, velit, debitis quis illum, voluptatem quas! Culpa repellendus tempore vitae tempora consequatur minima debitis ea dolorem ipsam reiciendis beatae dignissimos sunt itaque quasi maiores, quod eum repellat iusto ex vel, eius doloremque. Est cumque voluptates numquam eligendi quae vero perferendis at reprehend"
-            }
-          />
-          <Accordion
-            title={"ipsam reiciendis beatae dignissimos"}
+            title={"ipsam reiciendis beatae dignissimos sunt"}
             body={
               "quis illum, voluptatem quas! Culpa repellendus tempore vitae tempora consequatur minima debitis ea dolorem ipsam reiciendis beatae dignissimos sunt itaque quasi maiores, quod eum repellat iusto ex vel, eius doloremque. Est cumque voluptates numquam eligendi quae vero perferendis at reprehenderit nulla necessitatibus? Eum reprehenderit beatae odio et rerum exercitationem asperiores culpa quasi dolorum"
             }
           />
-
           <Accordion
-            title={"ipsam reiciendis beatae dignissimos"}
+            title={"ipsam reiciendis beatae dignissimos sunt"}
             body={
-              "quis illum, voluptatem quas! Culpa repellendus tempore vitae tempora consequatur minima debitis ea dolorem ipsam reiciendis beatae dignissimos sunt itaque quasi maiores, quod eum repellat iusto ex vel, eius doloremque. Est cumque voluptates numquam eligendi quae vero perferendis at reprehenderit nulla necessitatibus? Eum reprehenderit beatae odio et rerum exercitationem asperiores culpa quasi dolorum nostrum aspernatur consequatur esse sed eos, suscipit rem similique! Doloremque dolorum accusantium exercitationem nulla et ad voluptas eum deleniti consectetur ea, error cumque tenetur id molestiae beatae autem voluptatem nesciunt in rerum eos nemo laborum quae? Fugiat unde, consequatur itaque dolores numquam consequuntur incidunt soluta deserunt illo recusandae laboriosam iusto quod natus pariatur ut! Illum, nihil tempora rem ea sunt, consequuntur non libero voluptates iste quidem itaque culpa necessitatibus? Dignissimos aspernatur perspiciatis animi! Provident laborum quisquam ut dicta maiores voluptas dolorem assumenda illo dolor veritatis facilis sit totam tempora nulla dolores quasi reiciendis mollitia in deleniti, velit, debitis quis illum, voluptatem quas! Culpa repellendus tempore vitae tempora consequatur minima debitis ea dolorem ipsam reiciendis beatae dignissimos sunt itaque quasi maiores, quod eum repellat iusto ex vel, eius doloremque. Est cumque voluptates numquam eligendi quae vero perferendis at reprehend"
+              "quis illum, voluptatem quas! Culpa repellendus tempore vitae tempora consequatur minima debitis ea dolorem ipsam reiciendis beatae dignissimos sunt itaque quasi maiores, quod eum repellat iusto ex vel, eius doloremque. Est cumque voluptates numquam eligendi quae vero perferendis at reprehenderit nulla necessitatibus? Eum reprehenderit beatae odio et rerum exercitationem asperiores culpa quasi dolorum"
             }
           />
+          <Accordion
+            title={"ipsam reiciendis beatae dignissimos sunt"}
+            body={
+              "quis illum, voluptatem quas! Culpa repellendus tempore vitae tempora consequatur minima debitis ea dolorem ipsam reiciendis beatae dignissimos sunt itaque quasi maiores, quod eum repellat iusto ex vel, eius doloremque. Est cumque voluptates numquam eligendi quae vero perferendis at reprehenderit nulla necessitatibus? Eum reprehenderit beatae odio et rerum exercitationem asperiores culpa quasi dolorum"
+            }
+          />
+         
+
         </div>
       </main>
     </>
